8a92e2503fa680cf56c01db6215737f708df15d6
Henrique <henriqueos.92@outlook.com>
2025-03-10 15:16:36 -0300

    commit AZS-52, AZS-53, AZS-54, AZS-55, AZS-56

--- src/components/IndexNavBar.js
+++ src/components/IndexNavBar.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 import '../styles.css';
 
 const IndexNavBar = ({ projectName }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  
   return (
     <div className="index__navbar">
       <div className="project-name">{projectName}</div>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <FaBars />
+      </div>
+      {menuOpen && (
+        <div className="dropdown-menu">
+          <a href="http://azship/testlink/index.php" rel="TestLink">Acessar TestLink</a>
+        </div>
+      )}
     </div>
   );
 };
